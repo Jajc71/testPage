7486353abba8728ce02638ddffc28f8fac38f391
Abimael <abimaeljuarezc@gmail.com>
2024-07-09 16:41:12 -0600

    v0.5.5

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
-import { BodyComponent } from '../body/body.component';
+import { InicioComponent } from '../inicio/inicio.component';
+import { NosotrosComponent } from '../nosotros/nosotros.component';
+import { ProgramasComponent } from '../programas/programas.component';
+import { ContactoComponent } from '../contacto/contacto.component';
+import { CertificacionesComponent } from '../certificaciones/certificaciones.component';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [HeaderComponent, BodyComponent],
+  imports: [HeaderComponent, InicioComponent, 
+    NosotrosComponent, ProgramasComponent, 
+    CertificacionesComponent, ContactoComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
